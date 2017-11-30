efa439dc4774bbe9f8304990d866a8410a54d07c
Oxxyg33n <alexand3r.logvinenko@gmail.com>
2017-11-30 11:13:47 +0200

    Add basic twitter implementation

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,35 @@
 import org.telegram.telegrambots.*;
 import org.telegram.telegrambots.exceptions.*;
+import org.telegram.telegrambots.logging.BotLogger;
+import org.telegram.telegrambots.logging.BotsFileHandler;
+
+import java.io.IOException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
 
 public class Main {
+    private static final String LOGTAG = "Main";
+
     public static void main(String[] args) {
-        ApiContextInitializer.init();
-        TelegramBotsApi ninjaraBotApi = new TelegramBotsApi();
+        BotLogger.setLevel(Level.ALL);
+        BotLogger.registerLogger(new ConsoleHandler());
+        try {
+            BotLogger.registerLogger(new BotsFileHandler());
+        } catch (IOException e) {
+            BotLogger.severe(LOGTAG, e);
+        }
 
         try {
-            ninjaraBotApi.registerBot(new NinjaraBot());
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
+            ApiContextInitializer.init();
+            TelegramBotsApi ninjaraBotApi = new TelegramBotsApi();
+
+            try {
+                ninjaraBotApi.registerBot(new NinjaraBot());
+            } catch (TelegramApiException e) {
+                BotLogger.error(LOGTAG, e);
+            }
+        } catch (Exception e) {
+            BotLogger.error(LOGTAG, e);
         }
     }
 }
--- src/NinjaraBot.java
+++ src/NinjaraBot.java
@@ -1,22 +1,60 @@
 import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
+import twitter4j.*;
 
 public class NinjaraBot extends TelegramLongPollingBot {
+
     @Override
     public void onUpdateReceived(Update update) {
-        if(update.hasMessage() && update.getMessage().hasText()) {
-            SendMessage message = new SendMessage()         // Create a SendMessage object with mandatory fields
-                .setChatId(update.getMessage().getChatId())
-                .setText(update.getMessage().getText());
-            try {
-                execute(message);
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+
+        //check if the update has a message
+        if(update.hasMessage()){
+            Message message = update.getMessage();
+
+            //check if the message has text. it could also  contain for example a location ( message.hasLocation() )
+            if(message.hasText()){
+                if(message.getText().equals("Nintendo")) {
+
+                    Twitter twitter = TwitterFactory.getSingleton();
+                    try {
+                        Query query = new Query("#Nintendo");
+                        QueryResult result = twitter.search(query);
+                        SendMessage sendMessageRequest = new SendMessage();
+                        for (Status status : result.getTweets()) {
+                            sendMessageRequest.setChatId(update.getMessage().getChatId());
+                            sendMessageRequest.setText("@" + status.getUser().getScreenName() + ":" + status.getText());
+                            execute(sendMessageRequest);
+                        }
+
+                        Thread.sleep(3000);
+                    } catch (TwitterException te) {
+                        te.printStackTrace();
+                        System.out.println("Failed to search tweets: " + te.getMessage());
+                        try {
+                            Thread.sleep(3000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    } catch (InterruptedException | TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    SendMessage sendMessageRequest = new SendMessage();
+                    sendMessageRequest.setChatId(update.getMessage().getChatId());
+                    sendMessageRequest.setText("Sorry, I don't understand you, try writing: \"Nintendo\"");
+                    try {
+                        execute(sendMessageRequest);
+                    } catch (TelegramApiException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }//end if()
+        }//end  if()
+
+    }//end onUpdateReceived()
 
     @Override
     public String getBotUsername() {
